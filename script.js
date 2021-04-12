193914e3a4a5e315b909beee380e6eab0a75f5d1
anto991nio <antonio.91.cardone@gmail.com>
2021-04-12 14:04:55 +0200

    Termina esercizio

---
+++ script.js
@@ -0,0 +1,30 @@
+
+/* mi faccio un arrey che contenga tutti i miei 100 numeri */
+listaNumeri = [];
+
+/* con un ciclo for posso incrementare il mio arrey di un numero fino ad arrivare a 100 */
+for (var i = 1; i <= 100; i++) {
+
+    /* devinisco i valori all'interno dell'arrey in base ai multipli */
+    numero = i;
+    multiplo3 = numero % 3 === 0;
+    multiplo5 = numero % 5 === 0;
+    multipler35 = (numero % 3 === 0 && numero % 5 === 0);
+
+    if (multipler35) {
+        listaNumeri.push("FizzBuzz");
+    
+    } else if (multiplo5) {
+        listaNumeri.push("Buzz");
+
+    } else if (multiplo3) {
+        listaNumeri.push("Fizz");
+
+    } else {
+        listaNumeri.push(numero);
+    }
+}
+
+/* stampo la lista */
+console.log(listaNumeri);
+document.getElementById("listanumeri").innerHTML= "Ecco la lista dei numeri"+ " "+listaNumeri; 
